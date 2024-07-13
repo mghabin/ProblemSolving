593de64f5d22b9a64e7135a23390ff8e79199f18
mghabin <81494213+MohammadGhabin@users.noreply.github.com>
2024-07-13 20:54:20 +0300

    Introduce Stacks

---
+++ DataStructures/Stack/ArrayAsStack/ArrayAsStack.cs
@@ -0,0 +1,10 @@
+public namespace ProblemSolving.DataStructures.Stack.ArrayAsStack
+{
+    public class ArrayAsStack{
+        public void Push(){}
+
+        public int Pop(){}
+
+        public int Top(){}
+    }
+}
---
+++ DataStructures/Stack/LinkedListAsStack/LinkedListAsStack.cs
@@ -0,0 +1,18 @@
+public namespace ProblemSolving.DataStructures.Stack.LinkedListAsStack
+{
+    public class Node()
+    {
+        public Node Next { get; set; } = null;
+        public int Data { get; set; }
+
+        public Node(int data){
+            Data = data;
+        }
+    }
+
+    public class LinkedListAsStack
+    {
+        public LinkedListAsStack(){}
+    }
+
+}
