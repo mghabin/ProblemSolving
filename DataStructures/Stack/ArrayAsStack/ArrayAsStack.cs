44354925e9445d41f26df9da87b0c105c6fda533
mghabin <81494213+MohammadGhabin@users.noreply.github.com>
2024-07-18 16:39:19 +0300

    Stack implementation

--- DataStructures/Stack/ArrayAsStack/ArrayAsStack.cs
+++ DataStructures/Stack/ArrayAsStack/ArrayAsStack.cs
@@ -1,10 +1,82 @@
 public namespace ProblemSolving.DataStructures.Stack.ArrayAsStack
 {
-    public class ArrayAsStack{
-        public void Push(){}
+    public class ArrayAsStack
+    {
+        private int[] Elements;
 
-        public int Pop(){}
+        private int Top;
 
-        public int Top(){}
+        private int Max;
+
+        public ArrayAsStack(int size)
+        {
+            Elements = new int[size];
+            Max = size;
+            Top = -1;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public void Push(int data)
+        {
+            if (Top == Max - 1)
+            {
+                Console.WriteLine("Stack Overflow!");
+                return;
+            }
+            Elements[++Top] = data;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Is Empty!");
+                return -1;
+            }
+            return Elements[Top--];
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Is Empty!");
+                return -1;
+            }
+            return Elements[Top];
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public bool IsEmpty()
+        {
+            return Top == -1;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public bool IsFull()
+        {
+            return Top == Max - 1;
+        }
+
+        // Time Complexity: O(n).
+        // Auxiliary Space: O(1).
+        public void Print()
+        {
+            if (!IsEmpty())
+            {
+                for (int i = 0; i < Top - 1; i++)
+                {
+                    Console.Write($"{Elements[i]} -> ")
+                }
+                Console.Write($"{Elements[i]}")
+            }
+        }
     }
 }
--- DataStructures/Stack/LinkedListAsStack/LinkedListAsStack.cs
+++ DataStructures/Stack/LinkedListAsStack/LinkedListAsStack.cs
@@ -5,14 +5,57 @@ public namespace ProblemSolving.DataStructures.Stack.LinkedListAsStack
         public Node Next { get; set; } = null;
         public int Data { get; set; }
 
-        public Node(int data){
+        public Node(int data)
+        {
             Data = data;
         }
     }
 
     public class LinkedListAsStack
     {
-        public LinkedListAsStack(){}
-    }
+        private Node Head;
+        public LinkedListAsStack() { }
 
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public void Push(int data)
+        {
+            // Null
+            // A
+            // B -> A
+            // C -> B -> A
+            Node newNode = new Node(data);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                return -1;
+            }
+            int data = Head.Data;
+            Head = Head.Next;
+            return data;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public int Peek()
+        {
+            if (IsEmpty())
+                return -1;
+            return Head.Data;
+        }
+
+        // Time Complexity: O(1).
+        // Auxiliary Space: O(1).
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+    }
 }
